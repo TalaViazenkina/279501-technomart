12aac9d8244d2318a3dba0e3309fe63ba0a943a2
Natallia Viazenkina <bluewolf@tut.by>
2019-03-01 16:34:37 +0300

    Добавлено отображение добавления в корзину и в закладки

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ var toBasket = document.querySelector(".modal-adding");
 var toBasketOpen = document.querySelectorAll(".button-to-basket");
 var toBasketClose = toBasket.querySelector(".modal-close");
 var toBasketContinue = toBasket.querySelector(".button-continue");
+var basketInfo = document.querySelector(".header-basket");
+var basketTotal = basketInfo.querySelector(".header-basket-quality");
+
+var toBookmarkAdd = document.querySelectorAll(".button-to-bookmark");
+var bookmarkInfo = document.querySelector(".header-bookmark");
+var bookmarkTotal = document.querySelector(".header-bookmark-quality");
 
 var feedback = document.querySelector(".modal-feedback");
 var feedbackOpen = document.querySelector(".contacts-feedback");
@@ -61,6 +67,12 @@ for (var i = 0; i < toBasketOpen.length; i++) {
   toBasketOpen[i].addEventListener("click", function (evt) {
     evt.preventDefault();
     toBasket.classList.add("modal-show");
+    if(basketInfo.classList.contains("header-basket-full")) {
+      basketTotal.textContent++;
+    } else {
+      basketInfo.classList.add("header-basket-full");
+      basketTotal.textContent++;
+    }
   });
 }
 
@@ -85,6 +97,19 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+//отображение добавления в закладки
+for (var i = 0; i < toBookmarkAdd.length; i++) {
+  toBookmarkAdd[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if(bookmarkInfo.classList.contains("header-bookmark-full")) {
+      bookmarkTotal.textContent++;
+    } else {
+      bookmarkInfo.classList.add("header-bookmark-full");
+      bookmarkTotal.textContent++;
+    }
+  });
+}
 
 //Промо-слайдер
 //Переключение слайдера стрелками вперед-назад
